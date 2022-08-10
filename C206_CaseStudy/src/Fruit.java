13ad07c0f50faba819facd79e7535dcb5bb2c1e9
21041214 <21041214@DESKTOP-S0TH3BL>
2022-08-10 17:10:21 +0800

    These are the 2 classes added and it is extended to the 'menu.java'

---
+++ C206_CaseStudy/src/Fruit.java
@@ -0,0 +1,26 @@
+
+/**
+ * @author 21041214
+ *
+ */
+public class Fruit extends menu {
+	private String category;
+	
+	public Fruit(String ID, String name, String category, double price) {
+		// TODO Auto-generated constructor stub
+		super(ID, name, price);
+		this.category = category;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+	
+	public String toString() {
+		// Write your codes here
+		String output = super.toString();
+		output = String.format("%-63s %-20s", output, category);
+		return output;
+	}
+
+}
